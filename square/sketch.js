35243e4b098a3ea5f8e6b811d914d39576637159
Hamza Siddiqui <hamzasiddiqui205@gmail.com>
2020-08-20 21:52:27 -0400

    Add files via upload

---
+++ square/sketch.js
@@ -0,0 +1,53 @@
+let values = [];
+
+let i = 0;
+let j = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); //windowWidth
+  values = new Array(width);
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(height);
+    // values[i] = noise(i/100.0)*height;
+  }
+}
+
+function draw() {
+  background(255);
+  if (i < values.length) {
+    for (let j = 0; j < values.length - i - 1; j++) {
+      let a = values[j];
+      let b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+      }
+    }
+  } else {
+    console.log('finished');
+    noLoop();
+  }
+  i++;
+  for (let i = 0; i < values.length; i++) {
+    stroke(0);
+    rect(i, height - values[i], 10, 10);
+    // line(i, height, i, height - values[i]);
+  }
+  noStroke();
+  text("Bubble Sort!\n(Square Edition)", 170, 300);
+  // text(values.toString(), 10, 70);
+  textSize(40);
+  textFont('Courier New');
+  
+  text("\n\n    (click anywhere for more)", 170, 400);
+  // text(values.toString(), 10, 70);
+  textSize(80);
+  textFont('Courier New');
+}
+function mousePressed(){
+    window.open("https://editor.p5js.org/Hamza_sid/present/2lRGQtr-Yl", '_blank');
+}
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
